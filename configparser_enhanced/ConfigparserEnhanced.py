0567e53958f79a253ecae953882d2538f3571839
William McLendon <wcmclen@sandia.gov>
2021-02-01 17:34:08 -0700

    Initial commit

---
+++ configparser_enhanced/ConfigparserEnhanced.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+"""
+from __future__ import print_function
+
+import configparser
+import os
+import pprint
+import re
+import sys
+
+
+class ConfigparserEnhanced(object):
+    """
+
+    Args:
+        filename (str): The filename of the .ini file to load.
+        profile  (str): The profile or <section> in the .ini file to
+            process for an action list.
+
+    Attributes:
+        config  ()    : The data from the .ini file as loaded by configparser.
+        profile (str) : The profile section from the .ini file that is loaded.
+        actions (dict): The actions that would be processed when apply() is called.
+
+    """
+
+    def __init__(self, filename, profile):
+        self._file        = filename
+        self._profile     = profile
+        self._actions     = None
+        self._config      = None
+
+
+# TODO: Implement me!
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Setup app using setuptools.
+
+See: https://pypi.org/classifiers/ for classifiers
+"""
+
+from setuptools import setup
+
+setup(
+    name="configparser_enhanced",
+    version="0.0.1",
+    description="Environment Modules / LMOD helper",
+    url="https://gitlab-ex.sandia.gov/trilinos-devops-consolidation/code/ConfigparserEnhanced",
+    author="William McLendon",
+    author_email="wcmclen@sandia.gov",
+    packages=['configparser_enhanced'],
+    install_requires=['configparser'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Devops',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        ],
+    )
+
+
