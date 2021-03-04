e62f1333b23fe1a279aad3b27c3271b627864603
William McLendon <wcmclen@sandia.gov>
2021-03-04 12:49:20 -0700

    minor tweak

--- configparserenhanced/version.py
+++ configparserenhanced/version.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 # Note, when updating this, the docstring in ConfigParserEnhanced
-# should also be updated.
-# Todo: Can we automate that docstring? Can it be removed?
+# and in pyproject.toml should also be updated
 __version__ = "0.1.4"
 
 
