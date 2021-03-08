5a399309d818fcbfb6a8a454723bb739a0dce35c
William McLendon <wcmclen@sandia.gov>
2021-03-08 11:52:26 -0700

    update exec-clean script

--- wip/setenvironment/exec-clean.sh
+++ wip/setenvironment/exec-clean.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 find . -name "*.py?" -exec rm {} \;
+find . -name "*.*,cover" -exec rm {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
 find . -name ".DS_Store" -exec rm {} \;
 find . -name "._.DS_Store" -exec rm {} \;
