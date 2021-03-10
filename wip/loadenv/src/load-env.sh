d55b8ec7d41d799ffea05b26a996a0b286c12481
Jason M. Gates <jmgate@sandia.gov>
2021-03-10 16:14:52 -0700

    Add load-env.sh script

---
+++ wip/loadenv/src/load-env.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Ensure that this script is sourced.
+if [ "${BASH_SOURCE[0]}" == "${0}" ] ; then
+  echo "This script must be sourced."
+  exit 1
+fi
+
+# Ensure the python3 in the user's environment is high enough.
+#python_too_old=$(python3 -c 'import sys; print(sys.version_info < (3, 9))')     # Uncomment this block when we want to
+#if [[ "${python_too_old}" == "True" ]]; then                                    # enforce a particular Python version.
+#  echo "This script requires Python 3.9+."                                      #
+#  echo "Your current `python3` is only $(python3 --version)."                   #
+#  return 1                                                                      #
+#fi                                                                              #
+
+# Ensure that an argument is supplied.
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+if [ $# -eq 0 ]; then
+  ${script_dir}/src/LoadEnv.py --help                                            # Might need to change this help text slightly.
+  return 1
+fi
+
+# Pass the input on to LoadEnv.py to do the real work.
+${script_dir}/src/LoadEnv.py $@
+if [[ $? -ne 0 ]]; then
+  return $?
+fi
+
+# Source the generated script to pull the environment into the current shell.
+env_file="/tmp/load_matching_env.sh"
+if [ -f ${env_file} ]; then
+  source ${env_file}
+  rm ${env_file}
+else
+  echo "LoadEnv.py failed to generate ${env_file}."
+  echo "Unable to load the environment."
+  return 1
+fi
