18b5429d6dd77ce7adcd4ea8164e11b256195dc1
Jason M. Gates <jmgate@sandia.gov>
2021-04-19 11:23:50 -0600

    Clean Sphinx build each time

--- wip/loadenv/doc/make_html_docs.sh
+++ wip/loadenv/doc/make_html_docs.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+if [ -e html ]; then
+    rm -rf html
+fi
 sphinx-build -W -b html source/ html/
 if [ $? -eq 0 ]; then
     echo "\nIf you ran this on your local machine, you can open them with\n"
