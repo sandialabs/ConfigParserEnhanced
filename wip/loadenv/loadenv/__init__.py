7a6501d30b9660b566fa447184e7c46c9c62c1a3
Jason M. Gates <jmgate@sandia.gov>
2021-04-20 16:57:54 -0600

    ATTEMPTING TO ADD __INIT__ FILES

---
+++ wip/loadenv/loadenv/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- mode: python; py-indent-offset: 4; py-continuation-offset: 4 -*-
+"""
+Init script for the LoadEnv package
+"""
+from .version import __version__
+
+from .LoadEnv import LoadEnv
