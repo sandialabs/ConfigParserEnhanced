b69db21ecba8b4ecd46e9d823eef118dd43358a1
Evan Harvey <eharvey@sandia.gov>
2021-05-04 11:48:20 -0600

    Fix LMOD stdout/stderr prints

--- wip/setenvironment/setenvironment/ModuleHelper.py
+++ wip/setenvironment/setenvironment/ModuleHelper.py
@@ -122,8 +122,10 @@ try: # pragma: cover if on lmod
             raise error
 
         finally:
-            print(stdout)
-            print(stderr)
+            if not stdout and not stdout == "":
+                print(stdout)
+            if not stderr and not stderr == "":
+                print(stderr)
 
         # Check the module function output for errors
         stderr_ok = True
