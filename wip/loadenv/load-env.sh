e8cdaa4466dfe46c453ce02a8cb34bc95d898dcb
Evan Harvey <eharvey@sandia.gov>
2021-06-10 10:50:42 -0600

    load-env.sh: Remove python venv, check for interactive mode, and cleanup
      - The python venv was removed since it does not prevent python3 being
        removed when `module purge` exists in environment-specs.ini.
      - Prevent running load-env.sh within interactive mode since python3
        may not be available in the path due to `module purge`.
      - Add and call cleanup function to ensure all variables and tmp files
        are cleaned up.

--- wip/loadenv/load-env.sh
+++ wip/loadenv/load-env.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
 
+
+#### BEGIN runnable checks ####
+
 # Ensure that this script is sourced.
 if [ "${BASH_SOURCE[0]}" == "${0}" ] ; then
   echo "This script must be sourced."
   exit 1
 fi
+
+if [ ! -z $LOAD_ENV_INTERACTIVE_MODE ]; then
+  echo "!!! Error !!!"
+  echo "An environment is already loaded."
+  echo "Type \"exit\" before loading another environment."
+  echo "!!! Error !!!"
+  return 1
+fi
+
+#### END runnable checks ####
+
+
+
+#### BEGIN helper functions ####
+
+################################################################################
+# cleanup function
+#
+# This function should be called anytime this script returns control to the
+# caller.
+################################################################################
+function cleanup()
+{
+   [ -f .load_matching_env_loc ] && rm -f .load_matching_env_loc 2>/dev/null
+   [ ! -z ${env_file} ]          && rm -f ${env_file} 2>/dev/null; rm -f ${env_file::-2}rc 2>/dev/null
+
+   unset python_too_old script_dir ci_mode cleanup env_file
+   return 0
+}
+
+#### END helper functions ####
+
+
+
+#### BEGIN environment setup ####
 
 # Ensure the python3 in the user's environment and high enough.
 if [[ ! -z $(which python3 2>/dev/null) ]]; then
@@ -12,39 +50,26 @@ if [[ ! -z $(which python3 2>/dev/null) ]]; then
 else
   echo "This script requires Python 3.6+."
   echo "Please load Python 3.6+ into your path."
-  return 1
+  cleanup; return 1
 fi
 
 if [[ "${python_too_old}" == "True" ]]; then
   echo "This script requires Python 3.6+."
   echo "Your current python3 is only $(python3 --version)."
-  return 1
+  cleanup; return 1
 fi
 
 # Get the location to the Python script.
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
-# Create a virtual environment for running the LoadEnv tool
-if [[ ! -d "${script_dir}/virtual_env" ]]; then
-  python3 -m venv ${script_dir}/virtual_env
-fi
-
-if [[ $? -eq 0 && -e ${script_dir}/virtual_env/bin/activate ]]; then
-    source ${script_dir}/virtual_env/bin/activate
-else
-    echo "Error creating virtual_env directory"
-    return 1
-fi
-
 # Ensure that an argument is supplied.
 if [ $# -eq 0 ]; then
   cd ${script_dir} >/dev/null; python3 -E -s -m loadenv --help; cd - >/dev/null
-  deactivate
-  return 1
+  cleanup; return 1
 fi
 
+# Check for Continuous Integration mode.
 ci_mode=0
-# Pass the input on to LoadEnv.py to do the real work.
 if [[ "$1" == "--ci_mode" ]]; then
     ci_mode=1
     echo "!!! Warning !!!"
@@ -53,44 +78,44 @@ if [[ "$1" == "--ci_mode" ]]; then
     shift
 fi
 
+# Pass the input on to LoadEnv.py to do the real work.
 cd ${script_dir} >/dev/null; python3 -E -s -m loadenv $@; cd - >/dev/null
 if [[ $? -ne 0 ]]; then
-  deactivate
-  return $?
+  cleanup; return $?
 fi
-
-# Get out of the virtual environment
-deactivate
 
 # Source the generated script to pull the environment into the current shell.
 if [ -f .load_matching_env_loc ]; then
   env_file=$(cat .load_matching_env_loc)
-  rm -f .load_matching_env_loc
 
   if [ -f ${env_file} ]; then
-    echo "source ${env_file}" > ${script_dir}/virtual_env/.envrc
-    echo "rm -f ${env_file}" >> ${script_dir}/virtual_env/.envrc
-    echo "unset ci_mode python_too_old script_dir" >> ${script_dir}/virtual_env/.envrc
-    echo "echo; echo; echo" >> ${script_dir}/virtual_env/.envrc
-    echo "echo \"********************************************************************************\""  >> ${script_dir}/virtual_env/.envrc
-    echo "echo \"           E N V I R O N M E N T  L O A D E D  S U C E S S F U L L Y\"" >> ${script_dir}/virtual_env/.envrc
-    echo "echo \"********************************************************************************\""  >> ${script_dir}/virtual_env/.envrc
+    echo "source ${env_file}"                                                                          > ${env_file::-2}rc
+    echo "echo; echo; echo"                                                                           >> ${env_file::-2}rc
+    echo "echo \"********************************************************************************\""  >> ${env_file::-2}rc
+    echo "echo \"           E N V I R O N M E N T  L O A D E D  S U C E S S F U L L Y\""              >> ${env_file::-2}rc
+    echo "echo \"********************************************************************************\""  >> ${env_file::-2}rc
 
     # Enter subshell and set prompt by default
     if [[ $ci_mode -eq 0 ]]; then
-      echo "echo; echo; echo" >> ${script_dir}/virtual_env/.envrc
-      echo "echo \"********************************************************************************\""  >> ${script_dir}/virtual_env/.envrc
-      echo "echo \"          T Y P E  \"exit\"  T O  L E A V E  T H E  E N V I R O N M E N T\"" >> ${script_dir}/virtual_env/.envrc
-      echo "echo \"********************************************************************************\""  >> ${script_dir}/virtual_env/.envrc
-      echo "export PS1=\"(${@: -1}) $ \"" >> ${script_dir}/virtual_env/.envrc
-      /bin/bash --init-file ${script_dir}/virtual_env/.envrc -i
+      echo "echo; echo; echo"                                                                           >> ${env_file::-2}rc
+      echo "echo \"********************************************************************************\""  >> ${env_file::-2}rc
+      echo "echo \"          T Y P E  \"exit\"  T O  L E A V E  T H E  E N V I R O N M E N T\""         >> ${env_file::-2}rc
+      echo "echo \"********************************************************************************\""  >> ${env_file::-2}rc
+      echo "export PS1=\"(${@: -1}) $ \""                                                               >> ${env_file::-2}rc
+      echo "export LOAD_ENV_INTERACTIVE_MODE=\"True\""                                                  >> ${env_file::-2}rc
+      /bin/bash --init-file ${env_file::-2}rc -i
     else
-      source ${script_dir}/virtual_env/.envrc
+      # Intentionally do no invoke cleanup() if this exits such that artifacts in /tmp/$USER are preserved.
+      source ${env_file::-2}rc
     fi
 
   else
     echo "load_env.py failed to generate ${env_file}."
     echo "Unable to load the environment."
-    return 1
+    cleanup; return 1
   fi
 fi
+
+cleanup
+
+#### END environment setup ####
