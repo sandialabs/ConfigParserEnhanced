4ac75af8160b0dadb8c6e88b3181c5e9c8079734
Josh Braun <josbrau@sandia.gov>
2021-06-24 14:34:22 -0500

    Updated .gitlab-ci.yml

--- wip/genconfig/install_reqs.sh
+++ wip/genconfig/install_reqs.sh
@@ -5,6 +5,10 @@ pushd $PWD
 
 cd ${script_dir}
 rm -rf deps > /dev/null 2>&1; mkdir deps; cd deps
+git clone git@internal.gitlab.server:trilinos-devops-consolidation/code/ConfigParserEnhanced.git
+git clone git@internal.gitlab.server:trilinos-devops-consolidation/code/SetEnvironment.git
+git clone git@internal.gitlab.server:trilinos-devops-consolidation/code/DetermineSystem.git
+git clone git@internal.gitlab.server:trilinos-devops-consolidation/code/KeywordParser.git
 git clone git@internal.gitlab.server:trilinos-devops-consolidation/code/loadenv.git LoadEnv
 git clone git@internal.gitlab.server:trilinos-devops-consolidation/code/SetProgramOptions.git
 cd -
@@ -13,6 +17,10 @@ cd -
 rm loadenv > /dev/null 2>&1
 rm setprogramoptions > /dev/null 2>&1
 
+ln -s deps/SetEnvironment/setenvironment/ .
+ln -s deps/ConfigParserEnhanced/src/configparserenhanced/ .
+ln -s deps/DetermineSystem/determinesystem/ .
+ln -s deps/KeywordParser/keywordparser/ .
 ln -s deps/LoadEnv/loadenv/ .
 ln -s deps/SetProgramOptions/setprogramoptions/ .
 
