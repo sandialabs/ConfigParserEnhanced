11bed99eee7f9d1c8aba59baa6846c67daf5cde3
Josh Braun <josbrau@sandia.gov>
2021-06-25 14:22:58 -0500

    Inherit formatted message methods from keywordparser.FormattedMsg

--- wip/determinesystem/determinesystem/DetermineSystem.py
+++ wip/determinesystem/determinesystem/DetermineSystem.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 from configparserenhanced import ConfigParserEnhanced
+from keywordparser import FormattedMsg
 import re
 import socket
 import sys
 import textwrap
 
 
-class DetermineSystem:
+class DetermineSystem(FormattedMsg):
     """
     This small utility determines the system name from a
     ``supported_systems.ini`` file, the ``hostname``, and potentially the
@@ -162,59 +163,3 @@ class DetermineSystem:
             self.supported_systems_data = ConfigParserEnhanced(
                 self.supported_systems_file
             ).configparserenhanceddata
-
-    def get_formatted_msg(self, msg, kind="ERROR", extras=""):
-        """
-        This helper method handles multiline messages, rendering them like::
-
-            +=================================================================+
-            |   {kind}:  Unable to find alias or environment name for system
-            |            'machine-type-1' in keyword string 'bad_kw_str'.
-            +=================================================================+
-
-        Parameters:
-            msg (str):  The error message, potentially with multiple lines.
-            kind (str):  The kind of message being generated, e.g., "ERROR",
-                "WARNING", "INFO", etc.
-            extras (str):  Any extra text to include after the initial ``msg``.
-
-        Returns:
-            str:  The formatted message.
-        """
-        for idx, line in enumerate(msg.splitlines()):
-            if idx == 0:
-                msg = f"|   {kind}:  {line}\n"
-            else:
-                msg += f"|           {line}\n"
-        for extra in extras.splitlines():
-            msg += f"|   {extra}\n"
-        msg = "\n+" + "="*78 + "+\n" + msg + "+" + "="*78 + "+\n"
-        return msg
-
-    def get_msg_for_list(self, msg, item_list, kind="ERROR"):
-        """
-        Helper function to generate a message using a list. Produces a message
-        like the following::
-
-            +=================================================================+
-            |   {kind}:  {msg}
-            |     - {item_list[0]}
-            |     - {item_list[1]}
-            |     - ...
-            |     - {item_list[n]}
-            +=================================================================+
-
-        Parameters:
-            msg (str):  The error message to print.  Can be multiline.
-            item_list (list):  The list of items to print in the error message.
-            kind (str):  The kind of message being generated, e.g., "ERROR",
-                "WARNING", "INFO", etc.
-
-        Returns:
-            str:  The formatted message.
-        """
-        extras = ""
-        for item in item_list:
-            extras += f"  - {item}\n"
-        msg = self.get_formatted_msg(msg, kind=kind, extras=extras)
-        return msg
