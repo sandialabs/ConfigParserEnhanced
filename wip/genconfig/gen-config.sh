3c48750fe9e261002d26051f3016a7fd54a764fd
Josh Braun <josbrau@sandia.gov>
2021-06-25 14:40:03 -0600

    Progress save.

--- wip/genconfig/gen-config.sh
+++ wip/genconfig/gen-config.sh
@@ -50,7 +50,23 @@ echo "**************************************************************************
 echo "                B E G I N  L O A D I N G  E N V I R O N M E N T"
 echo "********************************************************************************"
 
-source $script_dir/load-env.sh --ci-mode $@
-
-echo "After sourcing load-env.sh"
+source ${script_dir}/load-env.sh --ci-mode $@
 #### END environment setup ####
+
+
+
+#### BEGIN configuration ####
+echo -e "\n\n********************************************************************************"
+echo "                      B E G I N  C O N F I G U R A T I O N"
+echo "********************************************************************************"
+
+# Get the location to the Python script.
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+if [[ $@ == *"--cmake-fragment"* ]]; then
+  cd ${script_dir} >/dev/null; python3 -E -s -m gen_config $@; cd - >/dev/null
+else
+  cd ${script_dir} >/dev/null
+  cmake $(python3 -E -s -m gen_config $@)
+  cd - >/dev/null
+fi
