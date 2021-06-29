bdc84f0f6de1c436fc6af057c9671b91a2313480
Josh Braun <josbrau@sandia.gov>
2021-06-29 17:01:04 -0500

    Added some logic for parsing /path/to/src call arguments

--- wip/genconfig/gen-config.sh
+++ wip/genconfig/gen-config.sh
@@ -43,6 +43,40 @@ if [ $# -eq 0 ]; then
   return 1
 fi
 
+# Get proper call args to pass to GenConfig python module. This does not include
+# the /path/to/src that is specified as the last positional argument when
+# --cmake-fragment is not specified. For example:
+#
+# $ source gen_config.sh \
+#     build_name_here \      <-|-- py_call_args=${@:1:$#-1}
+#     --force \              <-|                (all but last)
+#     /path/to/src
+#
+# $ source gen_config.sh \
+#     --cmake-fragment foo.cmake \  <-|-- py_call_args=$@
+#     build_name_here               <-|
+#
+if [[ $@ != *"--cmake-fragment"* ]]; then
+  if [ -d ${@: -1} ]; then
+    py_call_args=${@:1:$#-1}
+  else
+    echo "+==============================================================================+"
+    echo "|   ERROR:  A valid path to source was not specified as the last positional"
+    echo "|           argument. Please correct this like:"
+    echo "|"
+    echo "|           source gen-config.sh $1 \\"
+    shift
+    while [[ $# -gt 0 ]]; do
+      echo "|             $1 \\"
+      shift
+    done
+    echo "|"
+    echo "+==============================================================================+"
+  fi
+else
+  py_call_args=$@
+fi
+
 
 
 #### BEGIN environment setup ####
@@ -50,7 +84,8 @@ echo "**************************************************************************
 echo "                B E G I N  L O A D I N G  E N V I R O N M E N T"
 echo "********************************************************************************"
 
-source ${script_dir}/load-env.sh --ci-mode $@
+# Last call arg should be the /path/to/src, not used by load-env.sh
+source ${script_dir}/load-env.sh --ci-mode $py_call_args
 #### END environment setup ####
 
 
@@ -63,10 +98,15 @@ echo "**************************************************************************
 # Get the location to the Python script.
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
-if [[ $@ == *"--cmake-fragment"* ]]; then
-  cd ${script_dir} >/dev/null; python3 -E -s -m gen_config $@; cd - >/dev/null
-else
-  cd ${script_dir} >/dev/null
-  cmake $(python3 -E -s -m gen_config $@)
-  cd - >/dev/null
+cd ${script_dir} >/dev/null; python3 -E -s -m gen_config $py_call_args; cd - >/dev/null
+
+if [[ -f .bash_cmake_flags_from_gen_config ]]; then
+  echo "cmake \\" > .cmake_call
+  cat .bash_cmake_flags_from_gen_config >> .cmake_call
+  echo ${@: -1} >> .cmake_call
+
+  # Execute cmake
+  cmake_call=$(cat .cmake_call)
+  rm .cmake_call
+  eval $cmake_call
 fi
