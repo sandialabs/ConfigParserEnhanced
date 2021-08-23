38a0e10ab502341ca67a7e9d4d913181ae850b11
Josh Braun <josbrau@sandia.gov>
2021-08-23 10:42:33 -0600

    Added explicit set_program_options.exception_control_level = 5

--- wip/genconfig/gen_config.py
+++ wip/genconfig/gen_config.py
@@ -462,6 +462,7 @@ class GenConfig(FormattedMsg):
             self.set_program_options = SetProgramOptionsCMake(
                 filename=self.args.config_specs_file
             )
+            self.set_program_options.exception_control_level = 5
 
     def load_load_env(self):
         """
