d9ae2e7dc0796566c9621c4e08b2d07357d4faf2
Evan Harvey <eharvey@sandia.gov>
2021-09-08 10:47:22 -0600

    tests/verification_tests: Fix assertion typo in gcc-7.2.0-serial

--- wip/genconfig/tests/verification_tests/test_verify_trilinos_configs.py
+++ wip/genconfig/tests/verification_tests/test_verify_trilinos_configs.py
@@ -62,7 +62,7 @@ class Test_verify_rhel7_configs(unittest.TestCase):
                [self.assert_package_config_contains,
                 'set(Trilinos_ENABLE_COMPLEX_DOUBLE ON CACHE BOOL "from .ini configuration")'],
                [self.assert_package_config_contains,
-                'set(CMAKE_CXX_FLAGS "-Wall -Wno-clobbered -Wno-vla -Wno-pragmas -Wno-unknown-pragmas -Wno-unused-local-typedefs -Wno-literal-suffix -Wno-deprecated-declarations -Wno-misleading-indentation -Wno-int-in-bool-context -Wno-maybe-uninitialized -Wno-nonnull-compare -Wno-address -Wno-inline -Wno-unused-but-set-variable -Wno-unused-variable -Wno-unused-label -Werror -DTRILINOS_HIDE_DEPRECATED_HEADER_WARNINGS" CACHE String "from .ini configuration")'],
+                'set(CMAKE_CXX_FLAGS "-Wall -Wno-clobbered -Wno-vla -Wno-pragmas -Wno-unknown-pragmas -Wno-unused-local-typedefs -Wno-literal-suffix -Wno-deprecated-declarations -Wno-misleading-indentation -Wno-int-in-bool-context -Wno-maybe-uninitialized -Wno-nonnull-compare -Wno-address -Wno-inline -Wno-unused-but-set-variable -Wno-unused-variable -Wno-unused-label -Werror -DTRILINOS_HIDE_DEPRECATED_HEADER_WARNINGS" CACHE STRING "from .ini configuration")'],
                [self.assert_package_config_contains,
                 'set(TPL_ENABLE_ParMETIS OFF CACHE BOOL "from .ini configuration" FORCE)'],
                # TODO: [self.assert_common_mpi_disables],
