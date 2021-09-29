25fc19858f7d197a4de9fc7df08f241b6dee8d07
Evan Harvey <eharvey@sandia.gov>
2021-09-29 11:31:19 -0600

    Switch machine-type-4 configs to no-package-enables

--- wip/genconfig/tests/verification_tests/test_verify_trilinos_configs.py
+++ wip/genconfig/tests/verification_tests/test_verify_trilinos_configs.py
@@ -723,7 +723,7 @@ class Test_verify_machine-type-4_configs(unittest.TestCase, common_verify_helper
     def setUp(self):
         '''Comman data structures for all tests - done on a per-test basis'''
         self.config_verification_map = \
-             {'machine-type-4_cuda-10.1.243-gnu-8.3.1-spmpi-rolling_release_static_Volta70_Power9_no-asan_no-complex_no-fpic_mpi_pt_no-rdc_all':
+             {'machine-type-4_cuda-10.1.243-gnu-8.3.1-spmpi-rolling_release_static_Volta70_Power9_no-asan_no-complex_no-fpic_mpi_pt_no-rdc_no-package-enables':
               [
                [self.assert_gcc_version, 8, 3, 1],
                [self.assert_kokkos_nodetype, "cuda"],
@@ -739,7 +739,7 @@ class Test_verify_machine-type-4_configs(unittest.TestCase, common_verify_helper
                [self.assert_use_rdc, False],
                [self.assert_package_config_contains, 'set(TPL_ENABLE_Scotch OFF CACHE BOOL \"from .ini configuration\" FORCE)'],
               ],
-              'machine-type-4_cuda-10.1.243-gnu-8.3.1-spmpi-rolling_release_static_Volta70_Power9_no-asan_no-complex_no-fpic_mpi_pt_rdc_all':
+              'machine-type-4_cuda-10.1.243-gnu-8.3.1-spmpi-rolling_release_static_Volta70_Power9_no-asan_no-complex_no-fpic_mpi_pt_rdc_no-package-enables':
               [
                [self.assert_gcc_version, 8, 3, 1],
                [self.assert_kokkos_nodetype, "cuda"],
@@ -782,10 +782,15 @@ class Test_verify_machine-type-4_configs(unittest.TestCase, common_verify_helper
         self.stdoutRedirect.stop()
         # self.stderrRedirect.stop()
 
-    def test_machine-type-4_cuda_10_1_243_gnu_8_3_1_spmpi_rolling_release_static_Volta70_Power9_no_asan_no_complex_no_fpic_mpi_pt_no_rdc_all(self):
+    def test_machine-type-4_cuda_10_1_243_gnu_8_3_1_spmpi_rolling_release_static_Volta70_Power9_no_asan_no_complex_no_fpic_mpi_pt_no_rdc_no-package-enables(self):
         '''Check that the job setup for our python testing matches
            expectations'''
-        self.check_one_config('machine-type-4_cuda-10.1.243-gnu-8.3.1-spmpi-rolling_release_static_Volta70_Power9_no-asan_no-complex_no-fpic_mpi_pt_no-rdc_all')
+        self.check_one_config('machine-type-4_cuda-10.1.243-gnu-8.3.1-spmpi-rolling_release_static_Volta70_Power9_no-asan_no-complex_no-fpic_mpi_pt_no-rdc_no-package-enables')
+
+    def test_machine-type-4_cuda_10_1_243_gnu_8_3_1_spmpi_rolling_release_static_Volta70_Power9_no_asan_no_complex_no_fpic_mpi_pt_rdc_no-package-enables(self):
+        '''Check that the job setup for our python testing matches
+           expectations'''
+        self.check_one_config('machine-type-4_cuda-10.1.243-gnu-8.3.1-spmpi-rolling_release_static_Volta70_Power9_no-asan_no-complex_no-fpic_mpi_pt_rdc_no-package-enables')
 
 if __name__ == '__main__':
     unittest.main()  # pragma nocover
