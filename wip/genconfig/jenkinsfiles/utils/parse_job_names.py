43b4237a57135f4aaab6f802d7b533e252949967
Evan Harvey <eharvey@sandia.gov>
2022-05-23 10:06:31 -0600

    jenkinsfiles: Pull out additional params

--- wip/genconfig/jenkinsfiles/utils/parse_job_names.py
+++ wip/genconfig/jenkinsfiles/utils/parse_job_names.py
@@ -5,12 +5,14 @@
 
 import configparser
 import sys
+import re
 
 cfg = configparser.ConfigParser(strict=False)
 cfg.read(sys.argv[1])
+token=sys.argv[2]
 
 for s in cfg:
-    if s.startswith("JENKINS_JOBS_FROM_SERVER_JENKINS"):
+    if s.startswith("JENKINS_JOBS_FROM_SERVER_JENKINS_TRILINOS_JAAS_FOR_GROUP_TRILINOS"):
         for i in cfg[s]:
-            if i.startswith("job"):
+            if re.match(token.lower(), i) != None:
                 print(cfg[s][i])
