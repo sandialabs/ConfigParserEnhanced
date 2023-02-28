0bd99830cb2a7f034e52a8deecdf257c87d46202
Justin M. LaPre <jmlapre@sandia.gov>
2023-02-28 15:55:03 +0000

    update PATH in bash script

--- wip/genconfig/doc/make_html_docs.sh
+++ wip/genconfig/doc/make_html_docs.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+export PATH=/ascldap/users/trilinos/.local/bin:$PATH
+
 sphinx-build -W -b html source/ html/
 if [ $? -eq 0 ]; then
     echo "\nIf you ran this on your local machine, you can open them with\n"
